88a26d715c6e032a6811d6b44bb2d2010f9a513f
Kevin Wu <kevin.wu@live.com.my>
2017-12-01 21:00:56 +0800

    better form 1

--- Chatbot102/Chatbot102/Forms/LeaveForm.cs
+++ Chatbot102/Chatbot102/Forms/LeaveForm.cs
@@ -6,13 +6,40 @@ using System.Threading.Tasks;
 namespace Chatbot102.Forms
 {
     [Serializable]
+    [Template(TemplateUsage.NotUnderstood,
+        "Sorry, I didn’t get that.",
+        "Please try again.",
+        "My apologies, I didn’t understand '{0}'.",
+        "Excuse me, I didn’t quite get that.",
+        "Sorry, but I’m a chatbot and don’t know what '{0}' means.")]
     public class LeaveForm
     {
+        [Describe(Description = "Your name")]
+        [Prompt("Please tell me your name (current value: {})")]
         public string Name { get; set; }
+
+        [Describe(Description = "Your email")]
+        [Prompt("Please tell me your email (current value: {})")]
+        [Pattern(@".+@.+\..+")]
         public string Email { get; set; }
+
+        [Describe(Description = "The type of leave you want to apply")]
+        [Prompt("What type of leave do you want to apply? (current value: {})  {||}")]
         public LeaveType LeaveType { get; set; }
+
+        [Describe(Description = "The leave date starting from")]
+        [Prompt("What date will your leave start? (current value: {})")]
         public DateTime LeaveFrom { get; set; }
+
+        [Describe(Description = "The leave ending date")]
+        [Prompt("What date will your leave end? (current value: {})")]
         public DateTime LeaveTo { get; set; }
+
+        [Optional]
+        [Numeric(1, 5)]
+        [Describe(Description = "Rate the leave application service")]
+        [Prompt("Please rate how much you like our leave application service\n\n (Choose 1 to 5) (current value: {})")]
+        public int? Rating { get; set; }  // when select Optional, the value type must be nullable
 
         public IForm<LeaveForm> BuildForm()
         {
@@ -24,7 +51,6 @@ namespace Chatbot102.Forms
 
         async Task SaveLeaveFormAsync(IDialogContext context, LeaveForm leave_form)
         {
-            //string reply = String.Format("You applied for leave starting on {0} until {1}.", leave_form.LeaveFrom.ToString("dd-MMM-yyyy"), leave_form.LeaveTo.ToString("dd-MMM-yyyy"));
             string reply = "You leave is being processed.";
             await context.PostAsync(reply);
         }
